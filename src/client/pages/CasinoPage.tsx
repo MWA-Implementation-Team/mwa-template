25c3f9a05a68720136d346eec9f5339f1827bd28
vytskalt <vytskalt@protonmail.com>
2026-09-18 16:09:54 +0300

    bring back header

--- src/client/pages/CasinoPage.tsx
+++ src/client/pages/CasinoPage.tsx
@@ -12,8 +12,6 @@ export const casinoPage: Page<CasinoPageProps> = {
 function CasinoPage({}: CasinoPageProps) {
     return (
         <>
-            <Header />
-
             <RollComponent />
         </>
     );
--- src/client/ui/Header.tsx
+++ src/client/ui/Header.tsx
@@ -2,14 +2,17 @@ export default function Header() {
     return (
         <nav>
             <img src="/Logotipas.png" style={'width:200px;'} />
-            {/* <ul> */}
-            {/*   <li> */}
-            {/*     <a href="/">Home</a> */}
-            {/*   </li> */}
-            {/*   <li> */}
-            {/*     <a href="/login">Login</a> */}
-            {/*   </li> */}
-            {/* </ul> */}
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+                <li>
+                    <a href="/casino">Casino</a>
+                </li>
+            </ul>
         </nav>
     );
 }
